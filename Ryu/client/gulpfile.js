d327fbba748fec7a66b9010d94b16098a6d98431
lowjack1 <harshit11541@gmail.com>
2020-08-06 01:56:57 +0530

    update gulpfile.js

--- Ryu/client/gulpfile.js
+++ Ryu/client/gulpfile.js
@@ -4,38 +4,48 @@ const uglifycss = require('gulp-uglifycss');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
 
+const ARG_MAP = {
+    'prod': 'dist',
+    'dev': 'build',
+}
+
 // Copy vendor file from src to build
 gulp.task('vendors', function(){
-    return gulp.src(['src/vendors/**/*']).pipe(gulp.dest('build/static/vendors'));
+    let working_folder = getWorkingFolder();
+    return gulp.src(['src/vendors/**/*']).pipe(gulp.dest(`${working_folder}/static/vendors`));
 });
 
 // copy images from src to build
 gulp.task('images', function(){
-    return gulp.src(['src/images/*']).pipe(gulp.dest('build/static/images'));
+    let working_folder = getWorkingFolder();
+    return gulp.src(['src/images/*']).pipe(gulp.dest(`${working_folder}/static/images`));
 });
 
 gulp.task('css', function () {
+    let working_folder = getWorkingFolder();
     return gulp.src('src/css/*.css')
         .pipe(uglifycss({
         "uglyComments": true
     }))
-    .pipe(gulp.dest('build/static/css/'));
+    .pipe(gulp.dest(`${working_folder}/static/css/`));
 });
 
 gulp.task('js', function () {
+    let working_folder = getWorkingFolder();
     return gulp.src('src/js/*.js')
       .pipe(babel({
         presets: ['@babel/env']
       }))
       .pipe(uglify())
-      .pipe(gulp.dest('build/static/js/'));
+      .pipe(gulp.dest(`${working_folder}/static/js/`));
 });
 
 
 gulp.task('html', () => {
+    let working_folder = getWorkingFolder();
     return gulp.src('src/templates/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build/templates/'));
+    .pipe(gulp.dest(`${working_folder}/templates/`));
 });
 
 
@@ -46,3 +56,34 @@ gulp.task('watch', function() {
 	gulp.watch('src/templates/*.html', gulp.series(['html']));
 });
 gulp.task('default', gulp.series(['run', 'watch']));
+
+
+function argumentParser(arg_list) {
+    /**
+     * Parse command Line arguemnt
+     * @param: Command Line argument list
+     */
+
+    let arg = {}, a, opt, thisOpt, curOpt;
+    for (a = 0; a < arg_list.length; a++) {
+        thisOpt = arg_list[a].trim();
+        opt = thisOpt.replace(/^\-+/, '');
+
+        if (opt === thisOpt) {
+            // argument value
+            if (curOpt) arg[curOpt] = opt;
+            curOpt = null;
+        }
+        else {
+            // argument name
+            curOpt = opt;
+            arg[curOpt] = true;
+        }
+    }
+    return arg;
+}
+
+function getWorkingFolder() {
+    let working_env = argumentParser(process.argv).env || 'dev';
+    return ARG_MAP[working_env];
+}
